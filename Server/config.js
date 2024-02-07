f8ca91afaa266d0f138bf9bff6316d3a8ce3d142
Hasafif <147178688+Hasafif@users.noreply.github.com>
2024-02-07 08:19:59 -0500

    Update config.js

--- Server/config.js
+++ Server/config.js
@@ -1,7 +1,7 @@
 // APIs Credintals
 
 // GPT API
-const GPT_api_key = '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'
+const GPT_api_key = '******************'
 const GPT_CONFIG = {
   api_url: 'https://api.openai.com/v1/chat/completions',
   api_key:GPT_api_key,
@@ -14,22 +14,22 @@ const GPT_CONFIG = {
 // AskYourPdf API
 const ASK_CONFIG = {
   headers1:{
-    'x-api-key': 'ask_091df11bd8bb8d924465b10e464ffffe'
+    'x-api-key': '***************************'
   },
   headers2:{
     'Content-Type': 'application/json',
-    'x-api-key': 'ask_091df11bd8bb8d924465b10e464ffffe'
+    'x-api-key': '*******************************'
   }
 }
 
 // Scholar API
 const SCHOLAR_CONFIG = {
-  api_key:"74tprW6GKdTpz1VoTxfw3vkC"
+  api_key:"**************************"
 }
 
 
 // Semantic Scholar API
-const SEM_api_key = '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'
+const SEM_api_key = '***************************************'
 const SEMANTIC_CONFIG = {
   api_url:'https://api.semanticscholar.org/graph/v1/paper/search',
   api_key:SEM_api_key,
